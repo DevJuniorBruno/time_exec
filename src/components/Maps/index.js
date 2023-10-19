e4d70cac817a5fa34ba5c2668a5dfbe1f532b8b4
DevJuniorBruno <brunode2001@gmail.com>
2023-10-19 10:42:46 -0300

    repair bug

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -11,7 +11,6 @@ const MapPage = ()=> {
     <div className="container_map">     
        <div className="map">
        <iframe 
-       title="Google Maps"
        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.8231032812014!2d-46.627498525341764!3d-23.57479596208468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce597a5e987bdf%3A0x4b1213a971ee989f!2sR.%20Heitor%20Peixoto%2C%20237%20-%20Cambuci%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001543-000!5e0!3m2!1spt-BR!2sbr!4v1697038754361!5m2!1spt-BR!2sbr">
 
        </iframe>
